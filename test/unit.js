5e3242e0dad743d0e64fb341314bacac5c0a240d
Trey Shugart <treshugart@gmail.com>
2015-04-28 10:47:14 +1000

    Convert to use build module and gulp. Update source and tests to use ES6.

--- test/unit.js
+++ test/unit.js
@@ -1,15 +1,4 @@
-(function () {
-  'use strict';
+'use strict';
 
-  var files = window.__karma__.files;
-  var start = window.__karma__.start;
-  var tests = [];
-
-  Object.keys(files).forEach(function (file) {
-    if (file.indexOf('/unit/') > -1) {
-      tests.push(file);
-    }
-  });
-
-  require(tests, start);
-}());
+import './lib/polyfills';
+import './unit/index';
